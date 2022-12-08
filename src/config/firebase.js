c7e04546042103c47a950761bc8fa31d40ac3472
Mahmoud Bassam <mahmoudbassam@dev-malsmairat.home>
2022-12-08 23:16:51 +0300

    added firebase authentication

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBfEXAtqLNeQdvzBoGJweoScvnyu02wmMo',
+  authDomain: 'kanz-251ce.firebaseapp.com',
+  projectId: 'kanz-251ce',
+  storageBucket: 'kanz-251ce.appspot.com',
+  messagingSenderId: '232599328848',
+  appId: '1:232599328848:web:5bb074863b147e3b421955',
+};
---
+++ src/constants/errorMessages.js
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGE = {
+  'auth/email-already-in-use': 'Email is already in use, try different one.',
+  'auth/user-not-found':
+    'Email is not correct, user is not found in the system.',
+  'auth/wrong-password': 'Password is incorrect, please try again.',
+};
